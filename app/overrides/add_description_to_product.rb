db22b4c22265c23051d26d55973001db717462da
Willian Feltri <willian_feltri@hotmail.com>
2012-02-28 16:18:36 -0300

    refactoring the layout

---
+++ app/overrides/add_description_to_product.rb
@@ -0,0 +1,4 @@
+ Deface::Override.new(:virtual_path => "shared/_products",
+                      :name => "insert_description",
+                      :replace => "div.product-image",
+                      :text => "Descricao")
